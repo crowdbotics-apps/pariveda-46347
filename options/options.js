ff545cea8d0ba573cf2ba06bf500607bb0b654e4
Crowdbotics <team@crowdbotics.com>
2024-02-14 17:13:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pariveda-46347",
+  url: "https://pariveda-46347.botics.co",
+  api: "https://pariveda-46347.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
